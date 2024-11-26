8d950abe5a9ee4ec1c4f612fbacbd588de3e79c1
Ramanujam <159724240+vijayramanujam@users.noreply.github.com>
2024-11-27 00:53:48 +0530

    Add initialize function

--- arrays.cpp
+++ arrays.cpp
@@ -25,6 +25,21 @@ struct Array {
 int main() {
 
 
+    Array* arr = initialize();
+
     return 0;
+}
+
+Array* initialize() {
+
+    Array *arr = new Array; // create a pointer to Array structre
+
+    // initialize the structre
+    arr->capacity = 10;
+    arr->length = 0;
+    arr->p = new int[arr->capacity];
+
+    return *arr; // return this pointer to structre to main()
+
 }
 
