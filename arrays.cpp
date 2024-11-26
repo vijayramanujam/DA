6e8917884f02d7fd3354ba3a474057bda087672e
Ramanujam <159724240+vijayramanujam@users.noreply.github.com>
2024-11-27 00:48:40 +0530

    Create Array structre

---
+++ arrays.cpp
@@ -0,0 +1,30 @@
+
+
+/*
+Implementing Array as ADT
+
+Operations:
+
+*/
+
+// Forward declarations
+struct Array;
+
+
+
+// *******************************
+
+struct Array {
+
+    int *p; // pointer pointing to array on heap
+    int capacity; // Max number of elements an array can hold
+    int length; // To keep count of current number of elements in the array
+
+};
+
+int main() {
+
+
+    return 0;
+}
+
