54461ea56f0cab4c9e49e626fddfc45138515890
Ramanujam <159724240+vijayramanujam@users.noreply.github.com>
2024-11-28 00:50:28 +0530

    Implement create() that creates first node and returns the pointer to main()

--- linkedLists.cpp
+++ linkedLists.cpp
@@ -1,18 +1,27 @@
 
 
-/*
- * LinkedList as ADT
- *
- */
-
-
-
-
-
 #include<iostream>
+
+struct Node {
+    int data;
+    Node *next;
+};
+
+Node* create() {
+
+    Node *tmp = new Node;
+    tmp->data = 10;
+    tmp->next = nullptr;
+    return tmp;
+
+}
 
 
 int main() {
+
+    Node *first = create(); // creates our first node
+
+    std::cout << first->data << " " << first->next;
 
     return 0;
 }
