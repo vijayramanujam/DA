9c975d4920897cbbb46ed1406488341e8fcb9b00
Ramanujam <159724240+vijayramanujam@users.noreply.github.com>
2024-11-28 01:30:24 +0530

    complete revamp. Implement initialize() to start with a node and push() to insert element to linkedlist

--- linkedLists.cpp
+++ linkedLists.cpp
@@ -1,27 +1,51 @@
 
 
-#include<iostream>
+
+#include <iostream>
 
 struct Node {
+
     int data;
     Node *next;
 };
 
-Node* create() {
+Node* initialize() {
+
+    Node *head = new Node;
+    head->data = 0;
+    head->next = nullptr;
+
+    return head; // successfully created first node and returned the pointer to main()
+}
+
+
+void push(Node *head, int elem) { // element passed will be added to the end of the node
+
+    // lets create a temp new node for this element
 
     Node *tmp = new Node;
-    tmp->data = 10;
+    tmp->data = elem;
     tmp->next = nullptr;
-    return tmp;
+
+    // Our new node is ready to be pushed but where > Let's identify the last node in our LL.
+
+    Node *curr = head; // both first and curr pointing to same node (first node)
+
+    while (curr->next != nullptr) { // keep running we dont reach last node
+
+        curr = curr->next;
+    }
+
+    // Now, our curr node is pointing to the last node of our LL.
+
+    curr->next = tmp; // we made the last node of our current node point the new node that we just created.
 
 }
 
 
 int main() {
 
-    Node *first = create(); // creates our first node
-
-    std::cout << first->data << " " << first->next;
+    Node *head = initialize();
 
     return 0;
 }
